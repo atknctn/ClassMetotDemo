6b23f7229d0189c7140c4a91893ee71519691d8f
atknc <atknc@DESKTOP-9RL605R>
2022-08-18 16:59:58 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void ekle (Musteri musteri)
+        {
+            Console.WriteLine("Yeni Musteri Eklendi... \n" + musteri.Adi);
+        }
+        public  void listele (Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi);
+        }
+        public void sil (Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi... \n" + musteri.Adi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using ClassMetotDemo;
+
+MusteriManager musteriManager = new MusteriManager();
+
+Musteri musteri1 = new Musteri();
+
+musteri1.Adi = "Atakan";
+musteri1.Soyadi = "Çetin";
+musteri1.Telefon = "0 506 923 80 85";
+
+Musteri musteri2 = new Musteri();
+
+musteri2.Adi = "Mehmet Yasin";
+musteri2.Soyadi = "Cırak";
+musteri2.Telefon = "0 555 680 12 70";
+
+musteriManager.ekle(musteri1);
+
+Musteri[] musteriler = { musteri1, musteri2 };
+Console.WriteLine("Musteri Listesi...");
+foreach (var item in musteriler)
+{
+    Console.WriteLine(item.Adi + " " + item.Soyadi);
+}
+
+musteriManager.sil(musteri2);
+
+
+
+
